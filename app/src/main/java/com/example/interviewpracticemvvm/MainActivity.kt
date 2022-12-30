152d1276f63af7c9aef067eeddab010dc9a98406
alex <arcolistro@gmail.com>
2022-12-29 21:30:06 -0800

    Move functionality and layout to movie list fragment from main activity

--- app/src/main/java/com/example/interviewpracticemvvm/MainActivity.kt
+++ app/src/main/java/com/example/interviewpracticemvvm/MainActivity.kt
@@ -10,29 +10,9 @@ import com.example.interviewpracticemvvm.databinding.ActivityMainBinding
 import com.example.interviewpracticemvvm.viewmodel.MovieViewModel
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MovieViewModel
-    private lateinit var movieAdapter: MovieAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        prepareRecyclerView()
-        viewModel = ViewModelProvider(this)[MovieViewModel::class.java]
-        viewModel.getPopularMovies()
-        viewModel.observeMovieLiveData().observe(this, Observer { movieList ->
-            movieAdapter.setMovieList(movieList)
-        })
-    }
-
-
-    private fun prepareRecyclerView() {
-        movieAdapter = MovieAdapter()
-        binding.rvMovies.apply {
-            layoutManager = GridLayoutManager(applicationContext, 2)
-            adapter = movieAdapter
-        }
+        setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/example/interviewpracticemvvm/MovieListFragment.kt
+++ app/src/main/java/com/example/interviewpracticemvvm/MovieListFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.interviewpracticemvvm.adapter.MovieAdapter
+import com.example.interviewpracticemvvm.databinding.FragmentMovieListBinding
+import com.example.interviewpracticemvvm.viewmodel.MovieViewModel
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +19,33 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MovieListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentMovieListBinding
+    private lateinit var viewModel: MovieViewModel
+    private lateinit var movieAdapter: MovieAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_movie_list, container, false
+        )
+
+        prepareRecyclerView()
+        viewModel = ViewModelProvider(this)[MovieViewModel::class.java]
+        viewModel.getPopularMovies()
+        viewModel.observeMovieLiveData().observe(viewLifecycleOwner, Observer { movieList ->
+            movieAdapter.setMovieList(movieList)
+        })
         return inflater.inflate(R.layout.fragment_movie_list, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MovieListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MovieListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun prepareRecyclerView() {
+        movieAdapter = MovieAdapter()
+        binding.rvMovies.apply {
+            layoutManager = GridLayoutManager(activity, 2)
+            adapter = movieAdapter
+        }
     }
 }
