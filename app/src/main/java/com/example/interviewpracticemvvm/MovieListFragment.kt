c795eab42c05b720550f648292c281da02f7a86a
alex <arcolistro@gmail.com>
2023-01-13 13:52:40 -0800

    Fix "no adapter attached" error, app runs

--- app/src/main/java/com/example/interviewpracticemvvm/MovieListFragment.kt
+++ app/src/main/java/com/example/interviewpracticemvvm/MovieListFragment.kt
@@ -1,6 +1,8 @@
 package com.example.interviewpracticemvvm
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +19,8 @@ import com.example.interviewpracticemvvm.viewmodel.MovieViewModel
  * A simple [Fragment] subclass.
  * Use the [MovieListFragment.newInstance] factory method to
  * create an instance of this fragment.
+ *
+ * fixed 2 issues via question asked on StackOverflow: https://stackoverflow.com/questions/74968100/migrating-from-using-activity-to-fragment-getting-e-recyclerview-no-adapter-at
  */
 class MovieListFragment : Fragment() {
     private lateinit var binding: FragmentMovieListBinding
@@ -28,24 +32,38 @@ class MovieListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        Log.d(TAG,"onCreateView: called")
+
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_movie_list, container, false
         )
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG,"attaching Adapter")
         prepareRecyclerView()
+        Log.d(TAG,"prepareRecyclerView called")
+
         viewModel = ViewModelProvider(this)[MovieViewModel::class.java]
         viewModel.getPopularMovies()
+        Log.d(TAG,"getPopularMovies called")
+
         viewModel.observeMovieLiveData().observe(viewLifecycleOwner, Observer { movieList ->
             movieAdapter.setMovieList(movieList)
+            Log.d(TAG,"movieAdapter.setMovieList(movieList) called")
+
         })
-        return inflater.inflate(R.layout.fragment_movie_list, container, false)
     }
 
     private fun prepareRecyclerView() {
         movieAdapter = MovieAdapter()
         binding.rvMovies.apply {
-            layoutManager = GridLayoutManager(activity, 2)
+            layoutManager = GridLayoutManager(requireContext(), 2) //Tried activity; appCompatActivity wouldn't work
             adapter = movieAdapter
         }
+        Log.d(TAG,"adapter attached")
+
     }
 }
